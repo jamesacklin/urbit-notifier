b0794710ca349710af19a204f9f5405eb352464f
James Acklin <james.acklin@gmail.com>
2023-03-07 09:28:50 -0500

    basic channel and poke

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use serde::{Deserialize, Serialize};
+use serde_yaml::{self};
+use urbit_http_api::ShipInterface;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    ship_url: String,
+    ship_code: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config_file = "config.yml";
+    let f = std::fs::File::open(config_file).expect("Could not open ship config.");
+    let ship_config: Config = serde_yaml::from_reader(f).expect("Could not read ship config.");
+    let ship_interface = ShipInterface::new(&ship_config.ship_url, &ship_config.ship_code).unwrap();
+    let mut channel = ship_interface.create_channel().unwrap();
+    channel
+        .poke("hood", "helm-hi", &"This is a poke".into())
+        .ok();
+    channel.delete_channel();
 }
